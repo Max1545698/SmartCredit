9390c8906cdc321946a18f3594974aa216d38331
Maxim Rozhok <max15071999@gmail.com>
2018-10-31 22:48:52 +0200

    Add project files.

---
+++ SmartCredit/Controllers/CreditController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartCredit.Controllers
+{
+    public class CreditController : Controller
+    {
+        public ViewResult Main()
+        {
+            return View();
+        }
+    }
+}
---
+++ SmartCredit/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SmartCredit
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+            app.UseStaticFiles();
+            app.UseBrowserLink();
+           // app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: null,
+                    template: "{controller}/{action}",
+                    defaults: new { controller = "Credit", action = "Main" });
+            });
+        }
+    }
+}
